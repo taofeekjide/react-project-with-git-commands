abeaa06298c945788bd3b1683298618e7d617e1d
Tao02 <idowutaofeek16@gmail.com>
2025-06-03 22:17:30 +0100

    created contextAPI functionality to manage state

---
+++ simple-shopping-cart/src/context/context.jsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+export const ShoppingCartContext = createContext(null);
+
+function ShoppingCartProvider({ children }) {
+  return (
+    <ShoppingCartContext.Provider value={{}}>
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+}
+
+export default ShoppingCartProvider;
